96bc835e0a48e879b778f28474d410f13f80180d
David Banks <amoebae@gmail.com>
2020-11-14 09:10:20 +0000

    printing chars

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,12 @@ module Main where
 import System.Random (randomRIO)
 --import Lib
 
+
+lowercaseChars = ['a'..'z']
+
 main :: IO ()
 main = do
-  putStrLn "Hello, world!"
+  i <- randomRIO (0 :: Int, (length lowercaseChars) - 1)
+  let c = lowercaseChars !! i
+  putStrLn $ "Hello, world: " ++ (show c)
   return ()
