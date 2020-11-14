a27d03b07cb170e6b6350c0d8464fd369e3269ab
David Banks <amoebae@gmail.com>
2020-11-14 09:50:59 +0000

    use sequence to build list

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,21 @@ import System.Random (randomRIO)
 
 lowercaseChars = ['a'..'z']
 
+randomChar :: IO Char
+randomChar = do
+  i <- randomRIO (0 :: Int, (length lowercaseChars) - 1)
+  return $ lowercaseChars !! i
+
+f :: [IO Char]
+f = take 2 $ repeat randomChar
+
+
+-- sequence takes a list of actions of type `m a` -- [IO Char] in this case
+-- and re-wraps them so that the IO is OUTSIDE the list and forms a big container
+-- rather than a lot of small containers
 main :: IO ()
 main = do
-  i <- randomRIO (0 :: Int, (length lowercaseChars) - 1)
-  let c = lowercaseChars !! i
-  putStrLn $ "Hello, world: " ++ (show c)
+  x <- randomChar
+  let y = sequence (take 2 $ repeat randomChar)
+  y >>= putStrLn
   return ()
