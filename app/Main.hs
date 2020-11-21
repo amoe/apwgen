f345b648f6fc615658d2647b7998401873206d85
David Banks <amoebae@gmail.com>
2020-11-21 10:16:30 +0000

    need a way to figure weighted choice

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.Random (randomRIO)
+import qualified Data.Map as M
 --import Lib
 
 
@@ -22,9 +23,33 @@ f = take 2 $ repeat $ randomChar lowercaseChars
 -- sequence takes a list of actions of type `m a` -- [IO Char] in this case
 -- and re-wraps them so that the IO is OUTSIDE the list and forms a big container
 -- rather than a lot of small containers
-main :: IO ()
-main = do
+pwMain :: IO ()
+pwMain = do
   y <- randomChar uppercaseChars
   let y = sequence (take 2 $ repeat $ randomChar lowercaseChars)
   y >>= putStrLn
   return ()
+
+
+miniDictionary :: [String]
+miniDictionary = [
+  "yaw",
+  "midge",
+  "constraint",
+  "cost",
+  "lamented"
+  ]
+
+
+testSet :: [Integer]
+testSet = [1, 2, 2, 3]
+
+
+genHistogram :: [Integer] -> M.Map Integer Integer
+genHistogram [] = M.empty
+genHistogram (x:xs) = M.insertWith inc x 1 (genHistogram xs)
+  where inc _ oldValue = (+1) oldValue
+
+main :: IO ()
+main = do
+  putStrLn "foo"
